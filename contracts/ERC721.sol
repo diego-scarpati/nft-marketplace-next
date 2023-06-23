da7ac8bf4df875900a503bbcfe39210fa0e7a637
Diego Scarpati <diegoscarpati13@gmail.com>
2023-06-22 21:58:47 -0300

    First migration

---
+++ contracts/ERC721.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ERC721 {
+  mapping(uint256 => address) private _tokenOwner;
+  mapping(address => uint256) private _ownedTokensByAddress;
+
+  function _exists(uint256 tokenId) internal view returns(bool) {
+    return _tokenOwner[tokenId] != address(0);
+  }
+
+  function _mint(address to, uint256 tokenId) internal {
+    require(to != address(0), "the to address must not be 0");
+    require(!_exists(tokenId), "token already exists");
+    _tokenOwner[tokenId] = to;
+    _ownedTokensByAddress[to] ++;
+  }
+}
